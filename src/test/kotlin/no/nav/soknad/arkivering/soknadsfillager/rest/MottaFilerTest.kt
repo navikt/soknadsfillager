3ba4bd973cf7b673c5c1862630f341a2b3abead4
Sturle Helland <sturle.helland@nav.no>
2019-11-20 11:22:05 +0100

    lagt til test for mottak av liste med flere filer

--- src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/MottaFilerTest.kt
+++ src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/MottaFilerTest.kt
@@ -3,6 +3,7 @@ package no.nav.soknad.arkivering.soknadsfillager.rest
 import no.nav.soknad.arkivering.soknadsfillager.dto.FilElementDto
 import no.nav.soknad.arkivering.soknadsfillager.repository.FilRepository
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -28,13 +29,19 @@ class MottaFilerTest {
 		val minUuid= UUID.randomUUID().toString()
 		val minFil = "Dette er min andre streng"  //TODO bytte ut med blob
 		//val minFil = "src/test/resources/navlogo.pdf"
-		val minliste =
-			listOf<FilElementDto>(FilElementDto(minUuid, minFil))
+		val minliste = opprettMottattFilListeMedBareEnFil(minUuid, minFil)
 
 		this.mottaFiler.mottaFiler(minliste)
 
 		assertTrue(this.mittRepository.findByUuid(minUuid).isNotEmpty())
 
 	}
+	@Test
+	fun mottaEnListeAvFilerOgsjekkRitigAntallLagret(){
+		val minListe = opprett3Filer()
 
+		this.mottaFiler.mottaFiler(minListe)
+
+		assertEquals(3,mittRepository.count().toInt())
+	}
 }
