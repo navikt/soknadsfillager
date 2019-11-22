f5036657f80c95a2ba32963c17f67d643c792401
Sturle Helland <sturle.helland@nav.no>
2019-11-22 13:23:28 +0100

    Fjernet ubrukte metoder etter refakturering

--- src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/ObjectMother.kt
+++ src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/ObjectMother.kt
@@ -7,17 +7,12 @@ internal fun opprettEnUUid(): String = UUID.randomUUID().toString()
 
 internal fun opprettEnTekstFil(): String ="min tekst ${opprettEnUUid()}"
 
-internal fun opprettEnEnkelFil(): Pair<String, String> = Pair(opprettEnUUid(), opprettEnTekstFil())
 internal fun opprettEnFil():FilElementDto = FilElementDto(opprettEnUUid(), opprettEnTekstFil())
 
-internal fun opprettListeMedEnFil(uuid: String, fil : String): List<FilElementDto> = listOf(FilElementDto(uuid, fil))
-
-internal fun opprettMottattFilListeMedBareEnFil(minUuid: String, minFil: String): List<FilElementDto> = listOf<FilElementDto>(FilElementDto(minUuid, minFil))
+internal fun opprettListeMedEnFil(uuid: String, fil : String?): List<FilElementDto> = listOf(FilElementDto(uuid, fil))
 
 internal fun opprettListeAv3FilDtoer(): List<FilElementDto> = listOf<FilElementDto>(opprettEnFil(), opprettEnFil(), opprettEnFil())
 
 internal fun hentUtEnListeAvUuiderFraListeAvFilElementDtoer(list: List<FilElementDto>) = list.map(FilElementDto::uuid)
 
 internal fun endreListtilMutableList(list: List<String>): MutableList<String> = list.toMutableList()
-
-
