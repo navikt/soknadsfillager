49d493f5df3e1b9997475c03eb89ea7aafcd2c5f
Sturle Helland <sturle.helland@nav.no>
2019-11-22 08:55:30 +0100

    refakturert objectMother

--- src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/ObjectMother.kt
+++ src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/ObjectMother.kt
@@ -2,40 +2,30 @@ package no.nav.soknad.arkivering.soknadsfillager.rest
 
 import no.nav.soknad.arkivering.soknadsfillager.dto.FilElementDto
 import java.util.*
+import kotlin.random.Random
 
-internal fun opprettEnEnkelFil(): Pair<String, String> {
-    val minUuid = UUID.randomUUID().toString()
-    val minFil = "Min fil som skal hentes $minUuid"
-    return Pair(minUuid, minFil)
+internal fun opprettEnUUid(): String {
+    val uuid = UUID.randomUUID().toString()
+
+    return uuid
 }
+internal fun opprettEnTeks(): String {
+    val tekst = "min tekst ${opprettEnUUid()}"
+    return tekst}
+
+
+
+internal fun opprettEnEnkelFil(): Pair<String, String> = Pair(opprettEnUUid(), opprettEnTeks())
+internal fun opprettEnFil():FilElementDto = FilElementDto(opprettEnUUid(), opprettEnTeks())
+
 internal fun opprettListeMedEnFil(uuid: String, fil : String): List<FilElementDto> = listOf(FilElementDto(uuid, fil))
 
+internal fun opprettMottattFilListeMedBareEnFil(minUuid: String, minFil: String): List<FilElementDto> = listOf<FilElementDto>(FilElementDto(minUuid, minFil))
 
-internal fun opprettMottattFilListeMedBareEnFil(minUuid: String, minFil: String): List<FilElementDto> {
-    val minMotattFilIListe =
-            listOf<FilElementDto>(FilElementDto(minUuid, minFil))
-
-    return minMotattFilIListe
-}
-
-internal fun opprettListeAv3FilDtoer(): List<FilElementDto> {
-    val uuid1 = UUID.randomUUID().toString()
-    val uuid2 = UUID.randomUUID().toString()
-    val uuid3 = UUID.randomUUID().toString()
-    val fil1 = "fil$uuid1"
-    val fil2 = "fil$uuid2"
-    val fil3 = "fil$uuid3"
-
-    val mottattFil1 = FilElementDto(uuid1, fil1)
-    val mottattFil2 = FilElementDto(uuid2, fil2)
-    val mottaFiler3 = FilElementDto(uuid3, fil3)
-
-    val minListeAvMottatteFiler = listOf<FilElementDto>(mottattFil1, mottattFil2, mottaFiler3)
-
-    return minListeAvMottatteFiler
-}
+internal fun opprettListeAv3FilDtoer(): List<FilElementDto> = listOf<FilElementDto>(opprettEnFil(), opprettEnFil(), opprettEnFil())
 
 internal fun hentUtEnListeAvUuiderFraListeAvFilElementDtoer(list: List<FilElementDto>) = list.map(FilElementDto::uuid)
 
 internal fun endreListtilMutableList(list: List<String>): MutableList<String> = list.toMutableList()
+
 
