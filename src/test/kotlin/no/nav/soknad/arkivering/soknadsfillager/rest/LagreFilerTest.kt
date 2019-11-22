8242fe3bd3fdca722fab978c9cf53e0bfa0723e4
Sturle Helland <sturle.helland@nav.no>
2019-11-22 14:26:03 +0100

    tester at filer ikke legges til når fil er null

--- src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/LagreFilerTest.kt
+++ src/test/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/LagreFilerTest.kt
@@ -3,19 +3,26 @@ package no.nav.soknad.arkivering.soknadsfillager.rest
 import no.nav.soknad.arkivering.soknadsfillager.repository.FilRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.util.*
 
 
 @SpringBootTest
 class LagreFilerTest {
+    val minUuid = opprettEnUUid()
+    val mineFilerListe = opprettListeAv3FilDtoer()
     @Autowired
     private lateinit var lagreFiler: LagreFiler
 
     @Autowired
     private lateinit var mittRepository: FilRepository
+
+    @BeforeEach
+    private fun lagreListeAvFiler() {
+        this.lagreFiler.lagreFiler(mineFilerListe)
+    }
 
     @AfterEach
     fun ryddOpp() {
@@ -24,7 +31,6 @@ class LagreFilerTest {
 
     @Test
     fun enkelTestAvMottaFilerTjenste() {
-        val minUuid = opprettEnUUid()
         val liste = opprettListeMedEnFil(minUuid, opprettEnTekstFil())
 
         this.lagreFiler.lagreFiler(liste)
@@ -34,16 +40,13 @@ class LagreFilerTest {
 
     @Test
     fun lagreEnListeAvFilerOgsjekkRitigAntallLagret() {
-        val minListe = opprettListeAv3FilDtoer()
-
-        this.lagreFiler.lagreFiler(minListe)
+        this.lagreFiler.lagreFiler(mineFilerListe)
 
         assertEquals(3, mittRepository.count().toInt())
     }
 
     @Test
     fun erstatterFilMedGittUuidMedNyFil(){
-        val minUuid = opprettEnUUid()
         val forsteFilVersion = opprettEnTekstFil()
 
         val minForsteFilVersjonIliste = opprettListeMedEnFil(minUuid, forsteFilVersion)
@@ -56,5 +59,13 @@ class LagreFilerTest {
         this.lagreFiler.lagreFiler(minAndreFilVersjonIListe)
 
         assertEquals(andeFilVersjon, mittRepository.findById(minUuid).get().data)
+    }
+
+    @Test
+    fun skalIkkeKunneLagreEnFilDtoSomManglerFil(){
+        val listeMedFilElementDtoSomManglerFil = opprettListeMedEnFil(minUuid, null)
+
+        this.lagreFiler.lagreFiler(listeMedFilElementDtoSomManglerFil)
+        assertEquals(3 ,this.mittRepository.count().toInt())
     }
 }
