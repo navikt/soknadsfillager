e428c1d5728c79b8406c0dddaa77b685efef3601
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-02-07 12:58:30 +0100

    Using @RequestParam instead of @RequestBody on GetMapping

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/HentFiler.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/HentFiler.kt
@@ -4,18 +4,17 @@ import no.nav.soknad.arkivering.soknadsfillager.dto.FilElementDto
 import no.nav.soknad.arkivering.soknadsfillager.service.HentFilerService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HentFiler(private val hentFilerService: HentFilerService) {
-    private val logger = LoggerFactory.getLogger(javaClass)
+	private val logger = LoggerFactory.getLogger(javaClass)
 
-    @GetMapping("/filer")
-    fun hentFiler(@RequestBody filListe: List<String>):
-            List<FilElementDto> {
-        logger.info("Forsøker å hentet følgende filer:  $filListe")
+	@GetMapping("/filer")
+	fun hentFiler(@RequestParam ids: List<String>): List<FilElementDto> {
+		logger.info("Forsøker å hente følgende filer: $ids")
 
-        return hentFilerService.hentFiler(filListe)
-    }
+		return hentFilerService.hentFiler(ids)
+	}
 }
