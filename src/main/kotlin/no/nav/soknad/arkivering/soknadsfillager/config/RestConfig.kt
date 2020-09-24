6c303d73188573adbff7a4f4bfc933427f132cfa
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-09-24 11:21:30 +0200

    SD-1190 - Kunne motta søknadsfiler fra henvendelse. Kommentert ut logging av fileUser/fileWriter navn

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/config/RestConfig.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/config/RestConfig.kt
@@ -39,10 +39,10 @@ class WebSecurityConfig(private val config: AppConfiguration) : WebSecurityConfi
 	fun configureGlobal(auth: AuthenticationManagerBuilder) {
 
 		auth.inMemoryAuthentication().withUser(config.restConfig.fileUser).password("{noop}${config.restConfig.fileUserPassword}").roles("USER")
-		logger.info("Konfigurert fileUser=${config.restConfig.fileUser}")
+		//logger.info("Konfigurert fileUser=${config.restConfig.fileUser}")
 		if (!config.restConfig.fileUser.equals(config.restConfig.fileWriter, true)) {
 			auth.inMemoryAuthentication().withUser(config.restConfig.fileWriter).password("{noop}${config.restConfig.fileWriterPassword}").roles("USER")
-			logger.info("Konfigurert fileWriter=${config.restConfig.fileWriter}")
+			//logger.info("Konfigurert fileWriter=${config.restConfig.fileWriter}")
 		}
 		logger.info("Konfigurert authenticationManager")
 	}
