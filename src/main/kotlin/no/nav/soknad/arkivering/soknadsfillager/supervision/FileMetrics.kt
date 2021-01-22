47261f5d0817d19c86c985f0a50e35aecb72bee3
nils-arne <50109859+nils-arne@users.noreply.github.com>
2021-01-22 10:47:34 +0100

    SD-1162 - Merget inn fra main for å løse merge konflikter

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/supervision/FileMetrics.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/supervision/FileMetrics.kt
@@ -1,9 +1,6 @@
 package no.nav.soknad.arkivering.soknadsfillager.supervision
 
-import io.prometheus.client.CollectorRegistry
-import io.prometheus.client.Counter
-import io.prometheus.client.Histogram
-import io.prometheus.client.Summary
+import io.prometheus.client.*
 import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Scope
