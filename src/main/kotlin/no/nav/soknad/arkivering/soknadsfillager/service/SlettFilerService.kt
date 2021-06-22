5b4b921dd6a728b7bdb22be8d010c63b0c17ee3a
Johan Sjöblom <johan.sjoblom@squeed.com>
2021-06-22 16:49:08 +0200

    Performance improvement upon delete

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/service/SlettFilerService.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/service/SlettFilerService.kt
@@ -12,32 +12,27 @@ class SlettFilerService(private val filRepository: FilRepository, private val fi
 	private val logger = LoggerFactory.getLogger(javaClass)
 
 	fun slettFiler(filListe: List<String>) {
-
-		filListe.distinct().forEach {
-			if (!sjekkOmFilenEksisterer(it)) {
-				loggAtFilenMangler(it)
-			} else {
-				slettFil(it)
-			}
-		}
+		filListe.distinct().forEach { slettFil(it) }
 	}
-
-	private fun sjekkOmFilenEksisterer(uuid: String) = filRepository.findById(uuid).isPresent
 
 	private fun loggAtFilenMangler(uuid: String) = logger.error("$uuid er ikke i basen")
 
 	private fun slettFil(uuid: String) {
 		val fil = filRepository.findById(uuid)
+		if (!fil.isPresent) {
+			loggAtFilenMangler(uuid)
+			return
+		}
 
 		val oppdatertFil = FilDbData(uuid, null, fil.get().created)
 
 		val start = fileMetrics.filSummaryLatencyStart(Operations.DELETE.name)
 		val histogramTimer = fileMetrics.fileHistogramLatencyStart(Operations.DELETE.name)
+
 		try {
 			filRepository.saveAndFlush(oppdatertFil)
-			fileMetrics.filCounterInc(Operations.DELETE.name)
 
-			logger.info("Fil med $uuid er slettet fra basen")
+			fileMetrics.filCounterInc(Operations.DELETE.name)
 		} catch (error: Exception) {
 			fileMetrics.errorCounterInc(Operations.DELETE.name)
 			throw error
