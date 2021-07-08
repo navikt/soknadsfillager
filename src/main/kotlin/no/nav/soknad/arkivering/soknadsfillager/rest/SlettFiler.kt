e00dbeaf314997c7a9e48bab22aeb6bf4c5bcf51
Johan Sjöblom <johan.sjoblom@squeed.com>
2021-07-08 13:40:17 +0200

    Making Health endpoint go down when Exceptions occurr

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/SlettFiler.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/rest/SlettFiler.kt
@@ -1,5 +1,6 @@
 package no.nav.soknad.arkivering.soknadsfillager.rest
 
+import no.nav.soknad.arkivering.soknadsfillager.config.AppConfiguration
 import no.nav.soknad.arkivering.soknadsfillager.service.SlettFilerService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -7,13 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SlettFiler(private val slettFilerService: SlettFilerService) {
+class SlettFiler(private val slettFilerService: SlettFilerService, private val appConfig: AppConfiguration) {
 	private val logger = LoggerFactory.getLogger(javaClass)
 
 	@DeleteMapping("/filer")
 	fun slettFiler(@RequestParam ids: List<String>) {
-		logger.info("Skal slette '$ids'")
+		try {
+			logger.info("Skal slette '$ids'")
 
-		slettFilerService.slettFiler(ids)
+			slettFilerService.slettFiler(ids)
+
+		} catch (e: Exception) {
+			appConfig.applicationState.alive = false
+			logger.error("Exception occurred!", e)
+			throw e
+		}
 	}
 }
