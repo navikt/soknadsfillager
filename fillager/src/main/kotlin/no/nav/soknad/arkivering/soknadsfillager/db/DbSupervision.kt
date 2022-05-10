fb0352848cae5751843b2c40b912b7010107bb48
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-05-10 14:07:29 +0200

    Adding database supervision

---
+++ fillager/src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/db/DbSupervision.kt
@@ -0,0 +1,39 @@
+package no.nav.soknad.arkivering.soknadsfillager.db
+
+import no.nav.soknad.arkivering.soknadsfillager.repository.FilRepository
+import no.nav.soknad.arkivering.soknadsfillager.supervision.FileMetrics
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+@EnableScheduling
+class DbSupervision(
+	private val filRepository: FilRepository,
+	private val fileMetrics: FileMetrics
+) {
+
+	private val logger = LoggerFactory.getLogger(javaClass)
+
+	@Scheduled(cron = everyFiveMinutes)
+	fun databaseSupervisionStart() {
+		try {
+			collectDbStat()
+		} catch (e: Exception) {
+			logger.error("Something went wrong when performing database supervision", e)
+		}
+	}
+
+	private fun collectDbStat() {
+		val count = filRepository.count()
+		logger.info("Number of rows in the database: $count")
+
+		val documentCount = filRepository.documentCount()
+		logger.info("Number of rows in the database with documents: $documentCount")
+
+		fileMetrics.filesInDbGaugeSet(documentCount)
+	}
+}
+
+private const val everyFiveMinutes = "0 */5 * * * *"
