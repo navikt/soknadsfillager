e922f67e882b091bb2e46a1574d436d4052d5f18
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-09-01 17:55:42 +0200

    Minor cleanup

--- fillager/src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/SoknadsfillagerApplication.kt
+++ fillager/src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/SoknadsfillagerApplication.kt
@@ -1,13 +1,10 @@
 package no.nav.soknad.arkivering.soknadsfillager
 
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.openapitools.SpringDocConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
-import org.springframework.context.annotation.Profile
 
 @Import(SpringDocConfiguration::class)
 @SpringBootApplication
@@ -15,7 +12,5 @@ import org.springframework.context.annotation.Profile
 class SoknadsfillagerApplication
 
 	fun main(args: Array<String>) {
-
-
 		runApplication<SoknadsfillagerApplication>(*args)
 	}
