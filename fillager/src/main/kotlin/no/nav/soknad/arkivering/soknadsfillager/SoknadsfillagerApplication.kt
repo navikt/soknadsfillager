1ba28c3daeb215b186c2e38f79ceec3ae48d2f2a
Morten Stulen <morten.elias.stulen@nav.no>
2023-07-03 16:18:15 +0200

    Oppdater pakker

--- fillager/src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/SoknadsfillagerApplication.kt
+++ fillager/src/main/kotlin/no/nav/soknad/arkivering/soknadsfillager/SoknadsfillagerApplication.kt
@@ -2,15 +2,16 @@ package no.nav.soknad.arkivering.soknadsfillager
 
 import org.openapitools.SpringDocConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
 @Import(SpringDocConfiguration::class)
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 @ConfigurationPropertiesScan
 class SoknadsfillagerApplication
 
-	fun main(args: Array<String>) {
-		runApplication<SoknadsfillagerApplication>(*args)
-	}
+fun main(args: Array<String>) {
+	runApplication<SoknadsfillagerApplication>(*args)
+}
